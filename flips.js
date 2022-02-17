f1b7e9107c09e4fd6f9f9a7ac1e90c74fe2b58a4
NeilVakharia45 <neildvakharia@gmail.com>
2022-02-17 01:41:57 -0500

    Changed undefined logic

--- flips.js
+++ flips.js
@@ -4,9 +4,7 @@ import minimist from "minimist";
 // Call the coinFlip function and put the return into STDOUT
 var argv = minimist(process.argv.slice(2));
 var number=argv["number"]
-if(number==undefined){
-    number=1;
-}
+number = number || 1;
 
 var flips = coinFlips(number);
 console.log(flips);
