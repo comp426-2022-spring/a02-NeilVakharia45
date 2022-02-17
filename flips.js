4fb135d7734f1e1a74459247baf410b2dde4d7d9
NeilVakharia45 <neildvakharia@gmail.com>
2022-02-17 10:58:35 -0500

    Changed parse logic

--- flips.js
+++ flips.js
@@ -2,10 +2,11 @@
 import {coinFlip, coinFlips, countFlips } from "./modules/coin.mjs";
 import minimist from "minimist";
 // Call the coinFlip function and put the return into STDOUT
-var argv = minimist(process.argv.slice(2));
-var number=argv["number"]
-if(number==undefined){
-    number=1;
+
+var number = 1;
+if(process.argv.length >= 3){
+    var argv = minimist(process.argv.slice(2));
+    number=argv["number"]
 }
 
 var flips = coinFlips(number);
